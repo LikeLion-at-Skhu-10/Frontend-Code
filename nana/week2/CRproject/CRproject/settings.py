38863cbac2a8fee89329585233e6c14dd3e6badf
na hyun <lkg662130@naver.com>
2022-07-22 22:50:01 +0900

    FE_hw

--- nana/week2/CRproject/CRproject/settings.py
+++ nana/week2/CRproject/CRproject/settings.py
@@ -117,10 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+
+STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
@@ -129,5 +130,3 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-STATIC_URL = 'static/'
